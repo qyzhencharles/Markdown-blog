dafd2517e44d1d9702c0207a2518a40515604779
qyzhencharles <qyzhencharles@gmail.com>
2023-11-01 20:12:22 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const articleRouter = require('./routes/articles')
+const app = express()
+
+// mongoose.connect('mongodb://localhost/blog', {useNewUrlParser: true, useUnifiedTopology: true})
+
+app.set('view engine', 'ejs')
+
+// app.use(express.urlencoded({extended: false}))
+app.use('/articles',articleRouter)
+
+app.get('/', (req, res)=>{
+    const articles = [{
+        title: 'test article 1',
+        createdAt: new Date(),
+        description: 'test1'
+    },
+    {
+        title: 'test article 2',
+        createdAt: new Date(),
+        description: 'test2'
+    }
+]
+    res.render('articles/index', {articles: articles})
+
+})
+
+app.listen(9000)
